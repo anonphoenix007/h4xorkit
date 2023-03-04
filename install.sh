5c45c194de681bafbd76f289a1cdf1869ef9ef72
anonphoenix007 <118795606+anonphoenix007@users.noreply.github.com>
2023-03-04 13:40:44 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 if [[ $UID -eq 0 ]];then
 	apt update && apt upgrade -y
 	apt install nmap commix wifite python3 cupp john
+        mv -f server /usr/bin
+        chmod +x * && chmod +x /usr/bin/server
 elif [[ $UID -ne 0 ]];then
 	echo "H4xorkit will not run without root priviledge"
 	exit
